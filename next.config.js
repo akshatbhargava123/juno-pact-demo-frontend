4fe4907ddb5a150fe1d632448580506bfefe4460
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-17 07:53:08 +0530

    added export command to package file for netlify deployments

--- next.config.js
+++ next.config.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const {
   GRAPHQL_URI,
+  HASURA_ADMIN_SECRET,
 } = process.env;
 
 module.exports = {
   env: {
     GRAPHQL_URI,
+    HASURA_ADMIN_SECRET,
   },
   experimental: {
     jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
