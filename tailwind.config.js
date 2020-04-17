edee17eca09a4f4e1331d82d751e826ef1a85f37
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-17 13:57:29 +0530

    added temporary golden shades to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
     extend: {},
+    colors: {
+      ...defaultTheme.colors,
+      gold: {
+        200: '#D6C493',
+        400: '#B5963E'
+      },
+    },
   },
   variants: {},
   plugins: [],
