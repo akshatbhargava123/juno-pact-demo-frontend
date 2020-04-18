60828ee7831c0b284735729e96e26b87e0f76a9b
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-18 21:57:58 +0530

    nextjs pre-render indicator disabled in the config

--- next.config.js
+++ next.config.js
@@ -12,5 +12,8 @@ module.exports = {
   },
   experimental: {
     jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
-  }
+  },
+  devIndicators: {
+    autoPrerender: false,
+  },
 };
