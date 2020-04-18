8ab3069c3cf46be4ad25c359023d8f642ebf7642
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-19 05:24:23 +0530

    added Open-Sans as font dependency

--- components/common/page.js
+++ components/common/page.js
@@ -8,14 +8,7 @@ const Page = ({ title, children }) => {
 			<Head>
 				<title>{title} - JunoPact</title>
 				<link rel="icon" href="/favicon.ico" />
-				<link
-					rel="stylesheet"
-					href="https://cdn.jsdelivr.net/npm/font-proxima-nova@1.0.1/style.css"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://cdn.rawgit.com/mfd/f3d96ec7f0e8f034cc22ea73b3797b59/raw/856f1dbb8d807aabceb80b6d4f94b464df461b3e/gotham.css"
-				/>
+				<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/svg" sizes="32x32" href="/favicon.svg" />
 			</Head>
