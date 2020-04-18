814a432772bcabed0adf41e21ab5cc920b912b7c
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-18 23:48:57 +0530

    added autoFocus to DiputeSearch input

--- components/common/ProfileMenu/DisputeSearch.js
+++ components/common/ProfileMenu/DisputeSearch.js
@@ -5,6 +5,7 @@ const DisputeSearch = () => {
 		<div className="flex items-center border-b-2 border-blue-500">
 			<Search className="ml-2 m-0 text-gray-600" />
 			<input
+				autoFocus
 				placeholder="Search by Plaintiff / Defendant"
 				className="w-full outline-none h-10 px-3 py-2 text-lg"
 			/>
