42c9e75c08e1a10ad12e4c56ee3b53b3b5c1d756
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-18 21:05:52 +0530

    added MenuItems to dispute detail menu

--- components/common/DisputeDetailMenu/index.js
+++ components/common/DisputeDetailMenu/index.js
@@ -1,4 +1,12 @@
-import { Progress, CircularProgress, CircularProgressLabel } from '@chakra-ui/core';
+import { Progress } from '@chakra-ui/core';
+import { Edit, BookOpen, Archive, Database, Briefcase, Loader, Clock, Key, Folder } from 'react-feather';
+
+const MenuItem = ({ label, Icon }) => (
+	<div className="flex items-center p-2 cursor-pointer hover:bg-gray-300">
+		<Icon className="mr-4" />
+		<p>{label}</p>
+	</div>
+);
 
 const DisputeDetailMenu = () => {
 	return (
@@ -10,7 +18,18 @@ const DisputeDetailMenu = () => {
 				<p className="text-lg"><b>19 / 25</b> days left for final verdict.</p>
 				<Progress defaultValue={45} />
 			</div>
-			<div></div>
+			<div className="px-4">
+				<MenuItem label="Case History" Icon={Edit} />
+				<MenuItem label="Pleadings" Icon={Archive} />
+				<MenuItem label="Evidence" Icon={Database} />
+				<MenuItem label="Documents" Icon={BookOpen} />
+				<MenuItem label="E-filings" Icon={Briefcase} />
+				<MenuItem label="Orders" Icon={Loader} />
+				<MenuItem label="Timeline" Icon={Clock} />
+				<MenuItem label="Instructions" Icon={Key} />
+				<MenuItem label="Final Orders/Awards" Icon={BookOpen} />
+				<MenuItem label="Recorded Meetings" Icon={Folder} />
+			</div>
 		</div>
 	);
 };
