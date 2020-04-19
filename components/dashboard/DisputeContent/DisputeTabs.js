ca133e2240a26f88f8819a435113ddf8932a8dd6
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-19 10:34:19 +0530

    updated DisputeTabs design

--- components/dashboard/DisputeContent/DisputeTabs.js
+++ components/dashboard/DisputeContent/DisputeTabs.js
@@ -1,3 +1,5 @@
+import { Circle, Mail, Briefcase, GitPullRequest, Edit3 } from "react-feather";
+
 const DisputeTabsOptions = {
 	MY_MAILS: 'My Mails',
 	FILINGS: 'Filings',
@@ -5,26 +7,41 @@ const DisputeTabsOptions = {
 	NOTES: 'Notes',
 };
 
-const DisputeTab = ({ label, selected, onTabClick }) => (
+const DisputeTabsIcons = {
+	MY_MAILS: Mail,
+	FILINGS: Briefcase,
+	CORRESPONDENCE: GitPullRequest,
+	NOTES: Edit3,
+};
+
+const DisputeTab = ({ label, selected, onTabClick, Icon }) => (
 	<div
 		className={`
-			w-full py-3 cursor-pointer text-center hover:text-blue-700 hover:bg-blue-100 rounded-tl-lg
-			${selected ? 'bg-blue-100 text-blue-700' : 'bg-gray-100'}
+			w-full py-3 px-5 cursor-pointer text-white hover:bg-gray-700 flex justify-between
+			${selected ? 'bg-gray-700' : 'bg-gray-800 text-white'}
 		`}
 		onClick={onTabClick}
 	>
-	{label}
+		<p className="flex items-center">
+			<Icon className="mr-2" strokeWidth={selected ? '2px' : '1px'} />
+			{label}
+		</p>
+		<div className="flex items-center">
+			<Circle color="limegreen" size="10px" fill="limegreen" className="inline mr-3" />
+		</div>
 	</div>
 );
 
 const DisputeTabs = ({ selectedTab, onTabChange }) => {
 	const options = DisputeTabsOptions;
+	const icons = DisputeTabsIcons;
 	return (
 		<div className="flex justify-between">
 			{Object.keys(options).map(tabKey => (
 				<DisputeTab
 					key={tabKey}
 					label={options[tabKey]}
+					Icon={icons[tabKey]}
 					selected={selectedTab === options[tabKey]}
 					onTabClick={() => onTabChange(options[tabKey])}
 				/>
