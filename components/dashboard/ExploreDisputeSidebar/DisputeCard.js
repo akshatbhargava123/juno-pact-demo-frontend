54cb80fb3ffe8993d199978fe35c654923079bce
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-19 09:41:02 +0530

    updated DisputeCard design

--- components/dashboard/ExploreDisputeSidebar/DisputeCard.js
+++ components/dashboard/ExploreDisputeSidebar/DisputeCard.js
@@ -2,13 +2,12 @@ const DisputeCard = ({ plaintiff, defendant, category, lastUpdate }) => {
 	return (
 		<div className="rounded border-b border-gray-300 my-4 cursor-pointer hover:shadow-lg transition-duration-200">
 			<div className="px-6 py-3">
-				<p className="font-semibold">{`${plaintiff.name} VS ${defendant.name}`}</p>
+				<p className="">{`${plaintiff.name} VS ${defendant.name}`}</p>
 				<div className="font-light flex justify-between">
-					<p>{category}</p>
+					<p className="text-gray-700">{category}</p>
 					<p><span className="text-green-700 font-bold">5</span> / 24 days</p>
 				</div>
-				<div className="flex text-sm mt-1">
-					<p className="font-semibold mr-2">Last Update: </p>
+				<div className="flex text-gray-500 text-sm mt-1">
 					<p>{lastUpdate}</p>
 				</div>
 			</div>
