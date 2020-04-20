d25ac2fe355be65fef6b2c29c38de0f4820f68e8
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-20 12:28:31 +0530

    added search header to Notes section

--- components/dashboard/DisputeContent/Notes.js
+++ components/dashboard/DisputeContent/Notes.js
@@ -1,7 +1,16 @@
+import { Button } from "@chakra-ui/core"
+import SearchInput from "@components/common/SearchInput";
+
 const Notes = () => {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold flex-center">Notes</h1>
+		<div className="w-full">
+			<div className="flex justify-between">
+				<SearchInput
+					className="w-2/3"
+					placeholder="Search your Notes by title, description..."
+				/>
+				<Button onClick={() => console.log('hello')}>New Note</Button>
+			</div>
 		</div>
 	);
 };
