085f992c78da2169afd861b6399edec1e27fa1d2
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-20 16:27:35 +0530

    SearchInput now has a onChange callback

--- components/common/SearchInput/index.js
+++ components/common/SearchInput/index.js
@@ -1,6 +1,10 @@
 import { Search } from "react-feather";
 
-const SearchInput = ({ placeholder, className = '' }) => {
+const SearchInput = ({
+	placeholder,
+	className = '',
+	onChange = () => {},
+}) => {
 	return (
 		<div className={`shadow border flex items-center ${className}`}>
 			<Search className="ml-2 m-0 text-gray-500" />
@@ -8,6 +12,7 @@ const SearchInput = ({ placeholder, className = '' }) => {
 				autoFocus
 				placeholder={placeholder}
 				className="w-full outline-none h-10 px-3 py-2 text-lg"
+				onChange={e => onChange(e.target.value)}
 			/>
 		</div>
 	);
