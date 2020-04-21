b19611be0ca4d10575390b0abdcf9ac2e5185285
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-21 08:45:02 +0530

    added meta-charset utf-8

--- components/common/page.js
+++ components/common/page.js
@@ -11,6 +11,7 @@ const Page = ({ title, children }) => {
 				<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/svg" sizes="32x32" href="/favicon.svg" />
+				<meta charset="utf-8" />
 			</Head>
 			<div>
 				{children()}
