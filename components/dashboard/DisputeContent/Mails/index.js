72d1a711b83faeef9335e7871daeb77b9ac8299a
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-21 08:08:43 +0530

    added Search in Mailing section

--- components/dashboard/DisputeContent/Mails/index.js
+++ components/dashboard/DisputeContent/Mails/index.js
@@ -1,13 +1,32 @@
 import { Mail } from './Mail';
+import SearchInput from '@components/common/SearchInput';
+import { Button } from '@chakra-ui/core';
+import { Mail as MailIcon } from 'react-feather';
 
 const Mails = () => {
 	return (
-		<div>
-			<Mail />
-			<Mail />
-			<Mail read />
-			<Mail read />
-			<Mail read />
+		<div className="w-full">
+			<div className="flex justify-between">
+				<SearchInput
+					className="w-2/3 ml-6"
+					onChange={() => {}}
+					placeholder="Search mail"
+				/>
+				<Button
+					className="bg-blue-600 hover:bg-blue-300"
+					onClick={() => console.log('hello')}
+				>
+					Compose
+				</Button>
+			</div>
+
+			<div className="mt-10">
+				<Mail />
+				<Mail />
+				<Mail read />
+				<Mail read />
+				<Mail read />
+			</div>
 		</div>
 	);
 };
