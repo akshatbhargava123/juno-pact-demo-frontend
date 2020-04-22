18caa0105d445db903bd45ac60ab5e39560307cf
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-22 22:48:20 +0530

    updated email card design

--- components/dashboard/DisputeContent/Mails/Mail.js
+++ components/dashboard/DisputeContent/Mails/Mail.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Avatar } from '@chakra-ui/core';
+import { Avatar, Checkbox } from '@chakra-ui/core';
 import { Trash, Star, Eye } from 'react-feather';
 
 const Mail = ({ read }) => {
@@ -7,34 +7,38 @@ const Mail = ({ read }) => {
 	return (
 		<div
 			className={`
-				relative w-full h-16 rounded shadow px-10 flex items-center cursor-pointer
+				relative w-full h-16 rounded shadow px-10 pl-5 flex items-center cursor-pointer
 				${read ? 'bg-gray-200' : 'border'}
 			`}
 			onMouseOver={() => setIsHovered(true)}
 			onMouseOut={() => setIsHovered(false)}
 		>
+			<div className="p-4 flex-center rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
+				<Checkbox size="md" borderColor="gray.400" color="gray.900" />
+			</div>
+			<div className="p-2 mr-4 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
+				<Star strokeWidth="1px" />
+			</div>
 			<Avatar src="https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg" />
 			<div className="flex flex-col ml-4">
 				<p className="text-lg">Rahul Sharma</p>
 				<span className="text-gray-600">Mediator</span>
 			</div>
-			<div className="ml-10 w-3/5">
+			<div className="ml-10 w-3/6">
 				<p>Hey, Elon, I've been writing to you since last 10 months...</p>
 				<span className="text-gray-600">02:00 PM, 12th Dec, 2020</span>
 			</div>
-			{isHovered && (
-				<div className="absolute right-0 mr-10 flex">
-					<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
-						<Eye strokeWidth="1px" />
-					</div>
-					<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
-						<Star strokeWidth="1px" />
-					</div>
-					<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-red-500">
-						<Trash strokeWidth="1px" />
-					</div>
+			<div className="w-32">
+				<span className="text-gray-600 text-sm">2 attachments</span>
+			</div>
+			<div className="absolute right-0 flex mr-5" hidden={!isHovered}>
+				<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
+					<Eye strokeWidth="1px" />
 				</div>
-			)}
+				<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-red-500">
+					<Trash strokeWidth="1px" />
+				</div>
+			</div>
 		</div>
 	);
 };
