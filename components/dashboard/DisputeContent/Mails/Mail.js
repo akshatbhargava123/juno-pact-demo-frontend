9d53f0710bb7cc68b00ae90486417fe43bf72bf4
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-22 22:22:03 +0530

    added MailIcons to show on hovered state of MailCard

--- components/dashboard/DisputeContent/Mails/Mail.js
+++ components/dashboard/DisputeContent/Mails/Mail.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import { Avatar } from '@chakra-ui/core';
-import { Trash, Star } from 'react-feather';
+import { Trash, Star, Eye } from 'react-feather';
 
 const Mail = ({ read }) => {
+	const [isHovered, setIsHovered] = useState(false);
 	return (
-		<div className={`
-			relative w-full h-16 rounded shadow px-10 flex items-center cursor-pointer
-			${read ? 'bg-gray-200' : 'border'}
-		`}>
+		<div
+			className={`
+				relative w-full h-16 rounded shadow px-10 flex items-center cursor-pointer
+				${read ? 'bg-gray-200' : 'border'}
+			`}
+			onMouseOver={() => setIsHovered(true)}
+			onMouseOut={() => setIsHovered(false)}
+		>
 			<Avatar src="https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg" />
 			<div className="flex flex-col ml-4">
 				<p className="text-lg">Rahul Sharma</p>
@@ -16,14 +22,19 @@ const Mail = ({ read }) => {
 				<p>Hey, Elon, I've been writing to you since last 10 months...</p>
 				<span className="text-gray-600">02:00 PM, 12th Dec, 2020</span>
 			</div>
-			<div className="absolute right-0 mr-10 flex">
-				<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
-					<Star strokeWidth="1px" />
+			{isHovered && (
+				<div className="absolute right-0 mr-10 flex">
+					<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
+						<Eye strokeWidth="1px" />
+					</div>
+					<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
+						<Star strokeWidth="1px" />
+					</div>
+					<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-red-500">
+						<Trash strokeWidth="1px" />
+					</div>
 				</div>
-				<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-red-500">
-					<Trash strokeWidth="1px" />
-				</div>
-			</div>
+			)}
 		</div>
 	);
 };
