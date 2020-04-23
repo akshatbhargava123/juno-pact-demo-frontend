3dd37d78a0bdbb012cf4fed91959f2146d3a70cf
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-23 16:08:42 +0530

    added header menu icons

--- components/common/header.js
+++ components/common/header.js
@@ -1,22 +1,66 @@
-import { Avatar } from "@chakra-ui/core";
-import { ChevronDown } from "react-feather";
+import { Avatar, Popover, PopoverTrigger, PopoverContent, PopoverHeader } from "@chakra-ui/core";
+import { Box, ChevronDown, User, Plus, LogOut, Anchor, Info, Command } from "react-feather";
+
+const HeaderMenuItem = ({ text, Icon }) => (
+	<li className="cursor-pointer flex items-center text-sm my-1 p-2 mx-3 hover:text-blue-700">
+		<Icon size="20px" className="mr-2" />
+		<span>{text}</span>
+	</li>
+);
 
 const Header = () => {
 	return (
 		<div className="h-16 bg-gray-800 flex justify-between items-center px-10">
 			<img src="images/logo.svg" alt="logo" width="160px" />
-			<div className="flex items-center cursor-pointer rounded p-1 px-2 hover:bg-gray-700">
-				<Avatar
-					src="https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg"
-					name="Elon Musk"
-					size="sm"
-				/>
-				<span className="text-white text-xl ml-4 font-thin">Elon Musk</span>
-				<ChevronDown color="white" className="pt-px ml-1" />
-			</div>
-			<div className="font-semibold cursor-pointer bg-gold-400 hover:bg-gold-200 p-1 px-2 rounded transition duration-200">
-				Sign out
-			</div>
+			<Popover trigger="hover">
+				<PopoverTrigger>
+					<div className="flex items-center cursor-pointer rounded p-1 px-2 hover:bg-gray-700">
+						<Avatar
+							src="https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg"
+							name="Elon Musk"
+							size="sm"
+						/>
+						<span className="text-white text ml-4 font-thin">Elon Musk</span>
+						<ChevronDown color="white" className="pt-px ml-1" />
+					</div>
+				</PopoverTrigger>
+				<PopoverContent zIndex={50} width="16rem">
+					<PopoverHeader>
+						<p>Elon Musk</p>
+						<span className="text-gray-600 text-sm">elon@gmail.com</span>
+					</PopoverHeader>
+					<ul>
+						<HeaderMenuItem
+							text="Create New Dispute"
+							Icon={Plus}
+						/>
+						<HeaderMenuItem
+							text="My Profile"
+							Icon={User}
+						/>
+						<HeaderMenuItem
+							text="Help"
+							Icon={Info}
+						/>
+						<HeaderMenuItem
+							text="Support"
+							Icon={Box}
+						/>
+						<HeaderMenuItem
+							text="Keyboard Shortcuts"
+							Icon={Command}
+						/>
+						<HeaderMenuItem
+							text="Tour Juno Pact"
+							Icon={Anchor}
+						/>
+						<HeaderMenuItem
+							text="Logout"
+							Icon={LogOut}
+						/>
+					</ul>
+				</PopoverContent>
+			</Popover>
 		</div>
 	);
 };
