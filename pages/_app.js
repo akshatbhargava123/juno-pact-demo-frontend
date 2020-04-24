6b5679097ba4c7f669a157288598d241931e61ec
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-24 10:11:48 +0530

    added quill-js styles globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ApolloProvider } from '@apollo/react-hooks';
 import { client } from '@lib/init-apollo';
 import '../styles/index.css';
+import 'react-quill/dist/quill.snow.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
