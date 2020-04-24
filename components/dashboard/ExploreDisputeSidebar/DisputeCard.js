3532864658259997ce64f86a6b3513bb3519b15d
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-24 09:34:21 +0530

    showing some more info on DisputeCard

--- components/dashboard/ExploreDisputeSidebar/DisputeCard.js
+++ components/dashboard/ExploreDisputeSidebar/DisputeCard.js
@@ -1,5 +1,5 @@
 import useHover from "@components/common/hooks/useHover";
-import { Delete, Slash, Archive } from "react-feather";
+import { Mail, Sun } from "react-feather";
 
 const ActionButton = ({ text, color, Icon }) => (
 	<button className={`flex items-center mx-2 px-3 py-1 rounded bg-${color}-600 hover:bg-${color}-500 text-sm text-white font-semibold`}>
@@ -25,12 +25,19 @@ const DisputeCard = ({ plaintiff, defendant, category, lastUpdate }) => {
 						<p>{lastUpdate}</p>
 					</div>
 				</div>
-				<div className="w-56 flex flex-col items-end">
-					<p className="mr-5 py-1">
-						<span className="text-green-700 font-bold">5</span> / 24 days
-					</p>
-					<div className="flex items-center" hidden={!isHovered}>
-						<ActionButton text="Withdraw" color="red" Icon={Archive} />
+				<div className="w-64 flex flex-col items-end justify-start">
+					<div className="mr-5 py-1 align-top">
+						<p className="bg-gray-300 text-sm text-gray-700 px-2 py-1 rounded">Ongoing</p>
+					</div>
+					<div className="mr-5 py-1 flex" hidden={!isHovered}>
+						<p className="flex items-center bg-gray-600 text-sm text-white px-2 py-1 rounded mr-1">
+							<Mail strokeWidth="2px" size="1rem" className="mr-1" />
+							10
+						</p>
+						<p className="flex items-center bg-gray-600 text-sm text-white px-2 py-1 rounded">
+							<Sun strokeWidth="2px" size="1rem" className="mr-1" />
+							8
+						</p>
 					</div>
 				</div>
 			</div>
