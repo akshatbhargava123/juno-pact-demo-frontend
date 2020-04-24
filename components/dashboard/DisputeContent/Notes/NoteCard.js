12ba941d206a48e40921a9e1bd67725773c5263a
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-24 18:55:17 +0530

    added options to delete, draft mail and copy NoteCard on hover

--- components/dashboard/DisputeContent/Notes/NoteCard.js
+++ components/dashboard/DisputeContent/Notes/NoteCard.js
@@ -1,11 +1,34 @@
+import useHover from "@components/common/hooks/useHover";
+import { Trash2, Copy, Mail } from "react-feather";
+import HoverCircularEffect from "@components/common/HoverCircularEffect";
+
 const NoteCard = ({ title, description }) => {
+	const [hoverRef, isHovered] = useHover();
 	return (
 		<div
-			className="overflow-hidden w-48 border p-3 m-1 shadow-sm hover:shadow-md transition-duration-200 cursor-pointer"
+			ref={hoverRef}
 			style={{ maxHeight: '18.5rem', height: '14.5rem' }}
+			className="relative overflow-hidden w-48 border p-3 m-1 shadow-sm hover:shadow-md transition-duration-200 cursor-pointer"
 		>
 			<h1 className="font-semibold">{title}</h1>
 			<p className="text-sm">{description}</p>
+			<div className="absolute right-0 bottom-0 flex m-1" hidden={!isHovered}>
+				<HoverCircularEffect>
+					{({ isHovered }) => (
+						<Mail size="20px" className={`text-${isHovered ? 'blue' : 'gray'}-500`} />
+					)}
+				</HoverCircularEffect>
+				<HoverCircularEffect>
+					{({ isHovered }) => (
+						<Copy size="20px" className={`text-${isHovered ? 'blue' : 'gray'}-500`} />
+					)}
+				</HoverCircularEffect>
+				<HoverCircularEffect>
+					{({ isHovered }) => (
+						<Trash2 size="20px" className={`text-${isHovered ? 'red	' : 'gray'}-500`} />
+					)}
+				</HoverCircularEffect>
+			</div>
 		</div>
 	);
 };
