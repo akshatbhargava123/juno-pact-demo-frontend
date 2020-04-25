e44f4269ffd0b89e066c9d619b52c36f50f6633c
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-25 16:38:02 +0530

    MailCard icons wrapped with tooltips and hover effect

--- components/dashboard/DisputeContent/Mails/MailCard.js
+++ components/dashboard/DisputeContent/Mails/MailCard.js
@@ -16,7 +16,7 @@ const MailCard = ({ read }) => {
 			onMouseOut={() => setIsHovered(false)}
 		>
 			<Checkbox />
-			<HoverCircularEffect className="p-2 mr-4">
+			<HoverCircularEffect className="p-2 mr-4" tooltipOptions={{ label: 'Star' }}>
 				<Star strokeWidth="0px" fill="#F4B500" />
 			</HoverCircularEffect>
 			<Avatar src="https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg" />
@@ -32,12 +32,12 @@ const MailCard = ({ read }) => {
 				<span className="text-gray-600 text-sm">2 attachments</span>
 			</div>
 			<div className="absolute right-0 flex mr-5" hidden={!isHovered}>
-				<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-yellow-700 rounded-full">
-					<Eye strokeWidth="1px" />
-				</div>
-				<div className="p-2 rounded transition-duration-200 cursor-pointer hover:bg-gray-300 hover:text-red-500">
-					<Trash strokeWidth="1px" />
-				</div>
+				<HoverCircularEffect className="p-2" tooltipOptions={{ label: 'Mark read' }}>
+					{({ isHovered }) => <Eye strokeWidth="1px" className={`text-${isHovered ? 'yellow' : 'gray'}-700`} />}
+				</HoverCircularEffect>
+				<HoverCircularEffect className="p-2" tooltipOptions={{ label: 'Delete' }}>
+					{({ isHovered }) => <Trash strokeWidth="1px" className={`text-${isHovered ? 'red' : 'gray'}-700`} />}
+				</HoverCircularEffect>
 			</div>
 		</div>
 	);
