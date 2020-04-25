68e87fd29c4af49714a453dc3348fe80bbb8f98b
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-25 15:54:06 +0530

    HoverCircularEffect component now supports tooltip

--- components/common/HoverCircularEffect.js
+++ components/common/HoverCircularEffect.js
@@ -1,30 +1,40 @@
+import { Tooltip } from "@chakra-ui/core";
 import useHover from "@components/common/hooks/useHover";
 
 const HoverCircularEffect = ({
 	onParentClick,
 	children,
+	tooltipOptions,
 	hoverBgColor = 'gray',
 	className = 'p-2'
 }) => {
 	const [hoverRef, isHovered] = useHover();
+	tooltipOptions = tooltipOptions || {};
+	if (!tooltipOptions.label) {
+		tooltipOptions.isOpen = false;
+	}
 	return (
-		<div
-			tabIndex={0}
-			ref={hoverRef}
-			className={`
-				flex-center rounded transition-duration-200 cursor-pointer rounded-full outline-none
-				hover:bg-${hoverBgColor}-300 hover:text-red-500
-				focus:bg-${hoverBgColor}-300
-				active:bg-${hoverBgColor}-400
-				${className}
-			`}
-			onClick={(e) => {
-				if (onParentClick) onParentClick();
-				e.preventDefault();
-			}}
-		>
-			{typeof children === 'object' ? children : children({ isHovered })}
-		</div>
+		<Tooltip placement="top" hasArrow {...tooltipOptions}>
+			<div className="rounded-full">
+				<div
+					tabIndex={0}
+					ref={hoverRef}
+					className={`
+						flex-center rounded transition-duration-200 cursor-pointer rounded-full outline-none
+						hover:bg-${hoverBgColor}-300 hover:text-red-500
+						focus:bg-${hoverBgColor}-300
+						active:bg-${hoverBgColor}-400
+						${className}
+					`}
+					onClick={(e) => {
+						if (onParentClick) onParentClick();
+						e.preventDefault();
+					}}
+				>
+					{typeof children === 'object' ? children : children({ isHovered })}
+				</div>
+			</div>
+		</Tooltip>
 	);
 };
 
