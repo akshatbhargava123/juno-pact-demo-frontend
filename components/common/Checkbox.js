f7c40d44db0c8b66a38285fdfd22be4085df6058
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-25 16:00:21 +0530

    Checkbox component now takes tooltipOptions for it's wrapper

--- components/common/Checkbox.js
+++ components/common/Checkbox.js
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import { Checkbox as ChakraCheckbox } from '@chakra-ui/core';
 import HoverCircularEffect from '@components/common/HoverCircularEffect';
 
-const Checkbox = ({ defaultChecked = false }) => {
+const Checkbox = ({ defaultChecked = false, tooltipOptions = {} }) => {
 	const [checked, setChecked] = useState(defaultChecked);
 	return (
 		<HoverCircularEffect
-			onParentClick={() => setChecked(!checked)}
 			className="p-4"
+			tooltipOptions={tooltipOptions}
+			onParentClick={() => setChecked(!checked)}
 		>
 			<ChakraCheckbox
 				size="md"
