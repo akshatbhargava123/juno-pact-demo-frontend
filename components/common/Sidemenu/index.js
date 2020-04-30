20e2f391c451ca141a0f2bb6d93ff94468538efd
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-30 05:44:52 +0530

    added profile header for SideMenu

--- components/common/Sidemenu/index.js
+++ components/common/Sidemenu/index.js
@@ -1,7 +1,25 @@
+import { Edit3, Circle, ChevronDown } from 'react-feather';
+
 const SideMenu = () => {
 	return (
 		<div>
-			<h1>SideMenu</h1>
+			<div className="cursor-pointer border-t border-b border-sm border-gray-100">
+				<div className="flex justify-between items-center py-2 mx-3">
+					<div className="flex-col">
+						<h1 className="text-sm font-black text-white flex items-center">
+							<p>Juno Pact</p>
+							<ChevronDown color="white" size="1rem" className="pl-1" />
+						</h1>
+						<p className="text-xs flex items-center">
+							<Circle color="limegreen" size="10px" fill="limegreen" className="mr-2" />
+							Akshat Bhargava
+						</p>
+					</div>
+					<div>
+						<Edit3 className="text-lg h-8 w-8 p-2 text-gray-700 bg-white rounded-full" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
