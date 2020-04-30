ca0d700bd8095ab44e899b12dbaee1a92e10084f
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-30 16:37:29 +0530

    - updated hover effect for sidemenu items - started work on sidemenu dropdowns

--- components/common/Sidemenu/SideMenuItemContainer.js
+++ components/common/Sidemenu/SideMenuItemContainer.js
@@ -1,13 +1,13 @@
 import useHover from "@components/common/hooks/useHover";
 
-const SideMenuItemContainer = ({ children, selected }) => {
+const SideMenuItemContainer = ({ children, selected, hoverEffect = true }) => {
 	const [hoverRef, isHovered] = useHover();
 	return (
 		<div
 			ref={hoverRef}
 			className={`
 				flex items-center text-sm h-8 px-4 cursor-pointer
-				${selected ? 'text-gray-800 text-xs bg-gray-100' : 'hover:bg-blue-900'}
+				${selected ? 'text-white text-xs bg-blue-600' : hoverEffect && 'hover:bg-blue-900'}
 			`}
 		>
 			{typeof children === 'function' ? children({ isHovered }) : children}
--- components/common/Sidemenu/index.js
+++ components/common/Sidemenu/index.js
@@ -1,19 +1,44 @@
-import { Bookmark, Users } from 'react-feather';
+import { Bookmark, Users, Database, ChevronRight, Plus, MoreVertical } from 'react-feather';
 import { SideMenuUserControls } from './SideMenuUserControls';
+import { SideMenuItemContainer } from './SideMenuItemContainer';
+import { Collapse } from '@chakra-ui/core';
+import HoverCircularEffect from '../HoverCircularEffect';
 
 const SideMenu = () => {
 	return (
 		<div className="h-full bg-blue-800">
 			<SideMenuUserControls />
-			<div>
-				<div className="flex items-center text-sm hover:bg-blue-900 py-2 px-4 cursor-pointer">
+			<div className="py-3 border-b border-gray-600">
+				<SideMenuItemContainer>
 					<Bookmark size="16px" className="mr-2" />
 					<p>Saved Items</p>
-				</div>
-				<div className="flex items-center text-sm hover:bg-blue-900 py-2 px-4 cursor-pointer">
+				</SideMenuItemContainer>
+				<SideMenuItemContainer selected>
 					<Users size="16px" className="mr-2" />
 					<p>People</p>
-				</div>
+				</SideMenuItemContainer>
+				<SideMenuItemContainer>
+					<Database size="16px" className="mr-2" />
+					<p>Files</p>
+				</SideMenuItemContainer>
+			</div>
+			<div>
+				<SideMenuItemContainer hoverEffect={false}>
+					{({ isHovered}) => (
+						<div className="w-full flex items-center justify-between py-3">
+							<div className="flex items-center">
+								<ChevronRight size="1rem" className="mr-2 mt-1" />
+								<p>Channels</p>
+							</div>
+							<div className="flex items-center">
+								<HoverCircularEffect>
+									<MoreVertical size="20px" hidden={!isHovered} />
+								</HoverCircularEffect>
+								<Plus size="20px" className="ml-3" />
+							</div>
+						</div>
+					)}
+				</SideMenuItemContainer>
 			</div>
 		</div>
 	);
