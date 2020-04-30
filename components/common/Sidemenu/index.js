830f1b257d488f000d98ffb08edc6bc4c6365add
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-30 18:45:15 +0530

    added mock-ui sections in sidemenu

--- components/common/Sidemenu/index.js
+++ components/common/Sidemenu/index.js
@@ -1,11 +1,12 @@
 import { Collapse, useDisclosure } from '@chakra-ui/core';
 import HoverEffect from '@components/common/HoverEffect';
-import { Bookmark, Users, Database, ChevronRight, Plus, MoreVertical, ChevronDown } from 'react-feather';
+import { Bookmark, Users, Circle, Database, ChevronRight, Plus, MoreVertical, ChevronDown } from 'react-feather';
 import { SideMenuUserControls } from './SideMenuUserControls';
 import { SideMenuItemContainer } from './SideMenuItemContainer';
 
 const SideMenu = () => {
-	const { isOpen, onToggle } = useDisclosure(true);
+	const { isOpen: disputesOpen, onToggle: onDisputesToggle } = useDisclosure(true);
+	const { isOpen: personalOpen, onToggle: onPersonalToggle } = useDisclosure(true);
 	return (
 		<div className="h-full bg-blue-800">
 			<SideMenuUserControls />
@@ -24,12 +25,12 @@ const SideMenu = () => {
 				</SideMenuItemContainer>
 			</div>
 			<div className="py-3 border-b border-gray-600">
-				<SideMenuItemContainer onClick={onToggle} hoverEffect={false}>
+				<SideMenuItemContainer onClick={onDisputesToggle} hoverEffect={false}>
 					{({ isHovered }) => (
 						<div className="w-full flex items-center justify-between">
 							<div className="flex items-center">
-								{!isOpen && <ChevronRight size="1rem" className="mr-2 mt-1" />}
-								{isOpen && <ChevronDown size="1rem" className="mr-2 mt-1" />}
+								{!disputesOpen && <ChevronRight size="1rem" className="mr-2 mt-1" />}
+								{disputesOpen && <ChevronDown size="1rem" className="mr-2 mt-1" />}
 								<p>Disputes</p>
 							</div>
 							<div className="flex items-center">
@@ -43,11 +44,58 @@ const SideMenu = () => {
 						</div>
 					)}
 				</SideMenuItemContainer>
-				<Collapse isOpen={isOpen}>
+				<Collapse isOpen={disputesOpen}>
 					<SideMenuItemContainer><p className="ml-2">#&nbsp; Rahul VS Suneha</p></SideMenuItemContainer>
 					<SideMenuItemContainer><p className="ml-2">#&nbsp; Yes Bank VS Sahil</p></SideMenuItemContainer>
 					<SideMenuItemContainer><p className="ml-2">#&nbsp; Akshat VS Sahil</p></SideMenuItemContainer>
 					<SideMenuItemContainer><p className="ml-2">#&nbsp; Preeti VS Preetam</p></SideMenuItemContainer>
+				</Collapse>
+			</div>
+			<div className="py-3 border-b border-gray-600">
+				<SideMenuItemContainer onClick={onPersonalToggle} hoverEffect={false}>
+					{({ isHovered }) => (
+						<div className="w-full flex items-center justify-between">
+							<div className="flex items-center">
+								{!personalOpen && <ChevronRight size="1rem" className="mr-2 mt-1" />}
+								{personalOpen && <ChevronDown size="1rem" className="mr-2 mt-1" />}
+								<p>Personal</p>
+							</div>
+							<div className="flex items-center">
+								<HoverEffect tooltipOptions={{ label: 'Section Options' }} hidden={!isHovered}>
+									<MoreVertical size="1rem" />
+								</HoverEffect>
+								<HoverEffect tooltipOptions={{ label: 'Create New Dispute' }} hidden={!isHovered}>
+									<Plus size="1rem" />
+								</HoverEffect>
+							</div>
+						</div>
+					)}
+				</SideMenuItemContainer>
+				<Collapse isOpen={personalOpen}>
+					<SideMenuItemContainer>
+							<p className="ml-2 flex items-center">
+								<Circle color="limegreen" size="10px" fill="limegreen" className="mr-2" />
+								Suneha
+							</p>
+					</SideMenuItemContainer>
+					<SideMenuItemContainer>
+							<p className="ml-2 flex items-center">
+								<Circle color="limegreen" size="10px" fill="limegreen" className="mr-2" />
+								Sahil
+							</p>
+					</SideMenuItemContainer>
+					<SideMenuItemContainer>
+							<p className="ml-2 flex items-center">
+								<Circle color="limegreen" size="10px" fill="limegreen" className="mr-2" />
+								Shubham
+							</p>
+					</SideMenuItemContainer>
+					<SideMenuItemContainer>
+							<p className="ml-2 flex items-center">
+								<Circle color="limegreen" size="10px" fill="limegreen" className="mr-2" />
+								Preeti
+							</p>
+					</SideMenuItemContainer>
 				</Collapse>
 			</div>
 		</div>
