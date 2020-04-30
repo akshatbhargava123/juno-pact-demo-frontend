01116baa0218dbf23ed9fc80085bc695a01d15de
Akshat <akshatbhargavadeepa@gmail.com>
2020-04-30 19:13:22 +0530

    added design of ChannelHeader

--- components/dashboard/ChatSection/ChannelHeader.js
+++ components/dashboard/ChatSection/ChannelHeader.js
@@ -1,14 +1,24 @@
-import { Circle, Star } from "react-feather"
+import { Circle, Star, Info } from "react-feather"
 import HoverEffect from "@components/common/HoverEffect";
 
 const ChannelHeader = () => {
 	return (
-		<div className="h-16 bg-gray-200 text-gray-700 flex items-center justify-between px-5">
+		<div className="h-16 bg-white border-b text-gray-700 flex items-center justify-between px-5">
 			<div className="flex items-center">
 				<Circle color="limegreen" size="10px" fill="limegreen" className="mr-2 mt-1" />
 				<h4 className="text-sm font-bold">Shubham Kapaley</h4>
-				<HoverEffect tooltipOptions={{ label: 'Star conversation', placement: 'bottom' }}>
-					<Star size="0.75rem" className="cursor-pointer ml-2 mt-1" />
+				<HoverEffect tooltipOptions={{ label: 'Star conversation', placement: 'bottom' }} className="ml-2 p-1">
+					<Star size="0.75rem" className="cursor-pointer mt-1" />
+				</HoverEffect>
+			</div>
+			<div className="flex-center">
+				<HoverEffect className="p-2">
+					{({ isHovered }) => (
+						<div className={`flex items-center ${isHovered ? 'text-black' : 'text-gray-600'}`}>
+							<Info size="1rem" className="mt-px mr-2" />
+							<span className="text-sm">Details</span>
+						</div>
+					)}
 				</HoverEffect>
 			</div>
 		</div>
