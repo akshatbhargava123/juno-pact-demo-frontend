c4b7963bcafb734fcd26e790b0e4f6b2f1de172d
Akshat <akshatbhargavadeepa@gmail.com>
2020-05-01 13:45:46 +0530

    added custom key binding to chat input

--- components/dashboard/ChatSection/ChatInput.js
+++ components/dashboard/ChatSection/ChatInput.js
@@ -1,22 +1,48 @@
 import Quill from 'quill';
 import MagicUrl from 'quill-magic-url';
 import ReactQuill from 'react-quill';
+import { useRef, useEffect, useState } from 'react';
 
 Quill.register('modules/magicUrl', MagicUrl);
 
-const ChatInput = () => {
+const getQuillCustomModule = (shiftEnterHandler = () => {}) => ({
+	toolbar: [
+		['bold', 'italic', 'underline'],
+		['code-block', 'link'],
+	],
+	magicUrl: true,
+	keyboard: {
+		bindings: {
+			custom: {
+				key: 13,
+				handler: shiftEnterHandler,
+			},
+		},
+	},
+});
+
+const ChatInput = ({ onSend }) => {
+	const quillRef = useRef()
+	const [editorState, setEditorState] = useState('<p><strong>Hi</string><p>');
+	const [quillJSModules, setQuillJSModules] = useState();
+
+	const sendMessageHandler = () => {
+		console.log('send message')
+	};
+
+	useEffect(() => {
+		setQuillJSModules(getQuillCustomModule(sendMessageHandler));
+	}, []);
+
 	return (
 		<div className="h-full rounded border border-gray-600">
 			<ReactQuill
 				theme="snow"
-				modules={{
-					toolbar: [
-						['bold', 'italic', 'underline'],
-						['code-block', 'link'],
-					],
-					magicUrl: true,
-				}}
+				ref={quillRef}
+				value={editorState}
+				modules={quillJSModules}
 				placeholder="Message Satwik"
+				onChange={value => setEditorState(value)}
 			/>
 		</div>
 	);
