59d9028120344ccfcc3d8b041e34dd6e08997ac1
Akshat <akshatbhargavadeepa@gmail.com>
2020-05-01 10:57:00 +0530

    created separate component for PersonalChannelSection

--- components/dashboard/ChatSection/ChannelStartSection.js
+++ components/dashboard/ChatSection/ChannelStartSection.js
@@ -2,22 +2,28 @@ import { Avatar } from "@chakra-ui/core"
 import { Circle } from "react-feather";
 import UserBadge from "@components/common/UserBadge";
 
-const ChannelStartSection = ({ channelType }) => {
+const PersonalChannelSection = () => (
+	<div>
+		<div className="flex items-center">
+			<Avatar src="http://goog;e.csd" className="mr-5" />
+			<h4 className="flex flex-col font-bold text-sm">
+				<div className="flex items-center">
+					<p>Shubham Kapaley</p>
+					<Circle color="limegreen" size="10px" fill="limegreen" className="ml-2 mt-px" />
+				</div>
+				<UserBadge role="mediator" className="mr-4 mt-1" />
+			</h4>
+		</div>
+		<p className="mt-4 text-black">
+			You can discuss about anything with Shubham Kapaley here in personal.
+		</p>
+	</div>
+);
+
+const ChannelStartSection = ({ isGroupChannel = false }) => {
 	return (
 		<div className="absolute bottom-0 px-6 mb-2">
-			<div className="flex items-center">
-				<Avatar src="http://goog;e.csd" className="mr-5" />
-				<h4 className="flex flex-col font-bold text-sm">
-					<div className="flex items-center">
-						<p>Shubham Kapaley</p>
-						<Circle color="limegreen" size="10px" fill="limegreen" className="ml-2 mt-px" />
-					</div>
-					<UserBadge role="mediator" className="mr-4 mt-1" />
-				</h4>
-			</div>
-			<p className="mt-4 text-black">
-				You can discuss about anything with Shubham Kapaley here in personal.
-			</p>
+			{isGroupChannel ? <DisputeChannelSection /> : <PersonalChannelSection />}
 		</div>
 	);
 };
